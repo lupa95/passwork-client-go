6ba2d7f79067f4736d48029f5b1b9fc452b0521e
lupa95 <lupa95@web.de>
2024-02-17 19:39:26 +0100

    initial commit

---
+++ password_models.go
@@ -0,0 +1,94 @@
+package passwork
+
+type PasswordResponse struct {
+	Status string
+	Data   PasswordResponseData
+}
+
+type PasswordDeleteResponse struct {
+	Status string
+	Data   string
+}
+
+type PasswordSearchResponse struct {
+	Status string
+	Data   []PasswordResponseData
+}
+
+type PasswordResponseData struct {
+	VaultId            string
+	FolderId           string
+	Custom             []PasswordCustomData
+	Id                 string
+	Name               string
+	Login              string
+	CryptedPassword    string
+	CryptedKey         string
+	Description        string
+	Url                string
+	Color              int
+	Attachments        []PasswordAttachmentData
+	Tags               []string
+	Path               []PasswordPathData
+	Access             string
+	AccessCode         int
+	Shortcut           PasswordShortcutData
+	LastPasswordUpdate int
+	UpdatedAt          string
+	IsFavorite         bool
+}
+
+type PasswordSearchRequest struct {
+	Query         string   `json:"query"`
+	VaultId       string   `json:"vaultId"`
+	Colors        []int    `json:"colors,omitempty"`
+	Tags          []string `json:"tags,omitempty"`
+	IncludeShared bool     `json:"includeShared,omitempty"`
+}
+
+type PasswordRequest struct {
+	Name            string                   `json:"name"`
+	Login           string                   `json:"login,omitempty"`
+	CryptedPassword string                   `json:"cryptedPassword,omitempty"`
+	Url             string                   `json:"url,omitempty"`
+	Description     string                   `json:"description,omitempty"`
+	Custom          []PasswordCustomData     `json:"custom,omitempty"`
+	Color           int                      `json:"color,omitempty"`
+	Attachments     []PasswordAttachmentData `json:"attachments,omitempty"`
+	Tags            []string                 `json:"tags,omitempty"`
+	MasterHash      string                   `json:"masterHash,omitempty"`
+	VaultId         string                   `json:"vaultId"`
+	FolderId        string                   `json:"folderId,omitempty"`
+	ShortcutId      string                   `json:"shortcutId,omitempty"`
+}
+
+type PasswordCustomData struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+	Type  string `json:"type,omitempty"`
+}
+
+type PasswordAttachmentData struct {
+	Name          string `json:"name,omitempty"`
+	Id            string `json:"id,omitempty"`
+	EncryptedKey  string `json:"encryptedKey,omitempty"`
+	Hash          string `json:"hash,omitempty"`
+	EncryptedData string `json:"encryptedData,omitempty"`
+}
+
+type PasswordShortcutData struct {
+	Id         string
+	PasswordId string
+	VaultId    string
+	FolderId   string
+	Access     string
+	AccessCode int
+	CryptedKey string
+}
+
+type PasswordPathData struct {
+	Order int
+	Name  string
+	Type  string // Allowed: vault, folder, inbox
+	Id    string
+}
---
+++ passwords.go
@@ -0,0 +1,196 @@
+package passwork
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// Get a password by ID
+func (c *Client) GetPassword(pwId string) (PasswordResponse, error) {
+	url := fmt.Sprintf("%s/passwords/%s", c.BaseURL, pwId)
+	method := http.MethodGet
+	var responseObject PasswordResponse
+	var err error
+
+	// HTTP request
+	resp, err := c.sendRequest(method, url, nil)
+	if err != nil {
+		return responseObject, err
+	}
+
+	// Convert Body into byte stream
+	PasswordResponseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return responseObject, err
+	}
+
+	// Parse JSON into struct
+	err = json.Unmarshal(PasswordResponseData, &responseObject)
+	if err != nil {
+		return responseObject, err
+	}
+
+	if responseObject.Status != "success" {
+		return responseObject, err
+	}
+
+	return responseObject, nil
+}
+
+// Search for password by name
+func (c *Client) SearchPassword(request PasswordSearchRequest) (PasswordSearchResponse, error) {
+	url := fmt.Sprintf("%s/passwords/search", c.BaseURL)
+	method := http.MethodPost
+	var responseObject PasswordSearchResponse
+	var err error
+
+	body, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// HTTP request
+	resp, err := c.sendRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Convert Body into byte stream
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Parse JSON into struct (this returns a list of results)
+	err = json.Unmarshal(responseData, &responseObject)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	if responseObject.Status != "success" {
+		return responseObject, err
+	}
+
+	return responseObject, nil
+}
+
+func (c *Client) AddPassword(pwRequest PasswordRequest) (PasswordResponse, error) {
+	url := fmt.Sprintf("%s/passwords", c.BaseURL)
+	method := http.MethodPost
+	var responseObject PasswordResponse
+	var err error
+
+	body, err := json.Marshal(pwRequest)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// HTTP request
+	resp, err := c.sendRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Convert Body into byte stream
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Parse JSON into struct (this returns a list of results)
+	err = json.Unmarshal(responseData, &responseObject)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	if responseObject.Status != "success" {
+		return responseObject, err
+	}
+
+	return responseObject, nil
+}
+
+func (c *Client) EditPassword(pwId string, request PasswordRequest) (PasswordResponse, error) {
+	url := fmt.Sprintf("%s/passwords/%s", c.BaseURL, pwId)
+	method := http.MethodPut
+	var responseObject PasswordResponse
+
+	body, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// HTTP request
+	resp, err := c.sendRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Convert Body into byte stream
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Parse JSON into struct (this returns a list of results)
+
+	err = json.Unmarshal(responseData, &responseObject)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	if responseObject.Status != "success" {
+		return responseObject, err
+	}
+
+	return responseObject, nil
+}
+
+func (c *Client) DeletePassword(pwId string) (PasswordDeleteResponse, error) {
+	url := fmt.Sprintf("%s/passwords/%s", c.BaseURL, pwId)
+	method := http.MethodDelete
+	var responseObject PasswordDeleteResponse
+
+	// HTTP request
+	resp, err := c.sendRequest(method, url, nil)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Convert Body into byte stream
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	// Parse JSON into struct (this returns a list of results)
+	err = json.Unmarshal(responseData, &responseObject)
+	if err != nil {
+		log.Fatal(err)
+		return responseObject, err
+	}
+
+	if responseObject.Status != "success" {
+		return responseObject, err
+	}
+
+	return responseObject, nil
+}
