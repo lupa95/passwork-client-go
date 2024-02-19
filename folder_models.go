c9e59846d443c7ff3682927b77481c00562e4952
lupa95 <lupa95@web.de>
2024-02-19 21:01:32 +0100

    add folder models

--- folder_models.go
+++ folder_models.go
@@ -3,12 +3,17 @@ package passwork
 type FolderResponse struct {
 	Status string
 	Data   FolderResponseData
-	Code   string
+	Code   string // folderCreated, folderRenamed
 }
 
 type FolderSearchResponse struct {
 	Status string
 	Data   []FolderResponseData
+}
+
+type FolderDeleteResponse struct {
+	Status string
+	Data   string // folderDeleted
 }
 
 type FolderResponseData struct {
@@ -19,13 +24,14 @@ type FolderResponseData struct {
 	Path            PathData
 	FoldersAmount   int
 	PasswordsAmount int
+	ShortcutsAmount int
 	Access          int
 }
 
 type FolderAddRequest struct {
 	VaultId  string `json:"vaultId"`
 	Name     string `json:"name"`
-	ParentId string `json:"parentId"`
+	ParentId string `json:"parentId,omitempty"`
 }
 
 type FolderSearchRequest struct {
